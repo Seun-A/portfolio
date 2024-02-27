48a8d4336ffc921ff1a91745d439711149a46d70
Seun-A <73300414+Seun-A@users.noreply.github.com>
2024-02-27 08:58:32 +0100

    feat: project contributions

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,9 +3,18 @@ import Link from "next/link"
 import cardStyles from "../styles/projectCard.module.css"
 import { Icon } from "@iconify/react"
 
-export default function ProjectCard({ title, imageUrl, url }) {
+export default function ProjectCard({ title, imageUrl, url, isContribution }) {
   return (
     <Link href={url} target="_blank" className={cardStyles.card}>
+      {isContribution && (
+        <div className={cardStyles.contributionTag}>
+          <div className={cardStyles.tagCtn}>
+            <Icon icon="mingcute:group-2-fill" className={cardStyles.contributionIcon} />
+            <span>Contribution</span>
+          </div>
+        </div>
+      )}
+
       <div className={cardStyles.cardImgCtn}>
         <Image
           src={imageUrl}
