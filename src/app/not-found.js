5786da3543bccccd2e38eae804088101c7c14311
Seun-A <73300414+Seun-A@users.noreply.github.com>
2025-07-01 00:17:59 +0100

    fix: eslint build errors

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,9 @@
 'use client'
-import Image from 'next/image'
 import Link from 'next/link'
-// import building from '@/icons/building.svg'
  
 export default function NotFound() {
   return (
     <div className='flex flex-col items-center py-20'>
-      {/* <Image alt='in-progress' src={building} width={220} priority /> */}
       <h2 className='text-4xl mt-10 mb-5'>Page Not Found</h2>
       <Link href="/" className='text-peach text-2xl underline brightness-95 hover:brightness-100 transition-all'>Return Home</Link>
     </div>
--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,9 @@ import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
 import SkillsSection from "@/components/skills-section"
 import ProjectsSection from "@/components/projects-section"
-import ImpactSection from "@/components/impact-section"
+// import ImpactSection from "@/components/impact-section"
 import ExperienceSection from "@/components/experience-section"
-import TestimonialsSection from "@/components/testimonials-section"
+// import TestimonialsSection from "@/components/testimonials-section"
 import Footer from "@/components/footer"
 
 export default function Home() {
